f7607da21fffa29414f258108387240f9161b071
ole@kristensen.name <ole@kristensen.name@60b7121b-0056-0410-8572-c3939d7e8d03>
2008-09-16 22:16:13 +0000

    coordinate warping stuff sketched

---
+++ trunk/StopMotion/src/videoCamera.h
@@ -0,0 +1,29 @@
+/*
+ *  videoCamera.h
+ *  StopMotion2
+ *
+ *  Created by ole kristensen on 16/09/08.
+ *  Copyright 2008 tabla.dk. All rights reserved.
+ *
+ */
+
+#ifndef _VIDEO_CAMERA_H
+#define _VIDEO_CAMERA_H
+#include "coordWarping.h"
+
+class ofxVideoCamera : public ofSimpleApp {
+public:
+	
+	ofxPoint2f cornerNE, cornerSE, cornerSW, cornerNW;
+		
+	void setup();
+	void update();
+	void draw();
+	
+	ofxPoint2f getTransformedPoint(ofxPoint2f coordinate);
+	ofxPoint2f getUnTransformedPoint(ofxPoint2f coordinate);
+	int setCornerPoint(ofxPoint2f coordinate);
+	
+};
+
+#endif
