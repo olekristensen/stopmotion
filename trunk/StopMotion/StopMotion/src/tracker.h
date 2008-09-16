58ac844efa9f6bf514554838e0d30e5c7e2b39ce
jonas@halfdanj.dk <jonas@halfdanj.dk@60b7121b-0056-0410-8572-c3939d7e8d03>
2008-09-16 16:46:36 +0000

    import

---
+++ trunk/StopMotion/StopMotion/src/tracker.h
@@ -0,0 +1,27 @@
+class ofxTracker : public ofSimpleApp {
+public:
+	
+	void setup();
+	void update();
+	void draw();
+
+	ofxPoint2f getCurrentLocation();
+	
+	ofxPoint2f loc;
+	
+	ofVideoGrabber 		vidGrabber;
+	
+	ofxCvColorImage		colorImg;
+	
+	ofxCvGrayscaleImage 	grayImage;
+	ofxCvGrayscaleImage 	grayBg;
+	ofxCvGrayscaleImage 	grayDiff;
+	
+	ofxCvContourFinder 	contourFinder;
+	
+	int threshold;
+	bool pointMoved;
+	
+	ofTrueTypeFont	font;
+
+};
