9aacfa924bc825e6b8ad59132e50e36c71688ed0
ole@kristensen.name <ole@kristensen.name@60b7121b-0056-0410-8572-c3939d7e8d03>
2008-09-17 10:00:59 +0000

    commit before update of whole project

--- StopMotion/src/videoCamera.cpp
+++ StopMotion/src/videoCamera.cpp
@@ -5,6 +5,8 @@
  *  Created by ole kristensen on 16/09/08.
  *  Copyright 2008 tabla.dk. All rights reserved.
  *
+ *  HUSKAT gange med aspect i Y
+ *
  */
 
 #include "testApp.h"
@@ -16,12 +18,20 @@ void ofxVideoCamera::setup(){
 	cornerNW.set(-1.0f,-1.0f);
 }
 
-void ofxVideoCamera::update(){
-	
+void ofxVideoCamera::update(ofxPoint2f _currentLocation){
+	if(getTransformedPoint(_currentLocation)==ofxPoint2f(-1.0f,-1.0f)){
+		// der er ikke kalibreret endnu
+		if(setCornerPoint(ofxPoint2f _coordinate);
+	} else {
+		//lav alt Matriceopdateringen...
+	}
 }
 
 void ofxVideoCamera::draw(){
-	
+	if(getTransformedPoint(ofxPoint2f(0.0f,0.0f))==ofxPoint2f(-1.0f,-1.0f)){
+		// der er ikke kalibreret endnu
+	}
+	// tegn en grafik under kalibreringen ---
 }
 
 ofxPoint2f ofxVideoCamera::getTransformedPoint(ofxPoint2f _coordinate){
@@ -30,7 +40,7 @@ ofxPoint2f ofxVideoCamera::getTransformedPoint(ofxPoint2f _coordinate){
 	if(cornerNE==ofxPoint2f(-1.0f,-1.0f) || cornerSE==ofxPoint2f(-1.0f,-1.0f) || cornerSW==ofxPoint2f(-1.0f,-1.0f) || cornerNW==ofxPoint2f(-1.0f,-1.0f) ){
 		return ofxPoint2f(-1.0f,-1.0f);
 	} else {
-		
+		//lav alt Matriceopslag...
 	}
 }
 
@@ -49,16 +59,16 @@ int ofxVideoCamera::setCornerPoint(ofxPoint2f _coordinate){
 	//find which corner we're in and return an int representing the corner, if it was within the limits,
 	//else return a -1;
 	
-	if(_coordinate.x > 0.6f && _coordinate.y > 0.6f){
+	if(_coordinate.x > 0.6f && _coordinate.y > 0.6f*ASPECTRATIO){
 		cornerNE.set(_coordinate);
 		return 1;
-	} else if(_coordinate.x > 0.6f && _coordinate.y < 0.4f){
+	} else if(_coordinate.x > 0.6f && _coordinate.y < 0.4f*ASPECTRATIO){
 		cornerSE.set(_coordinate);
 		return 2;
-	} else if(_coordinate.x < 0.4f && _coordinate.y < 0.4f){
+	} else if(_coordinate.x < 0.4f && _coordinate.y < 0.4f*ASPECTRATIO){
 		cornerSW.set(_coordinate);
 		return 3;
-	} else if(_coordinate.x < 0.4f && _coordinate.y > 0.6f){
+	} else if(_coordinate.x < 0.4f && _coordinate.y > 0.6f*ASPECTRATIO){
 		cornerNW.set(_coordinate);
 		return 4;
 	} else {
