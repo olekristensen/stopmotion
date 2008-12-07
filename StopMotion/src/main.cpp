3bfa83ed9a8aa9cbd4ea65acf0079161f51edf02
ole@kristensen.name <ole@kristensen.name@60b7121b-0056-0410-8572-c3939d7e8d03>
2008-12-07 17:48:32 +0000

    new version with applescript interface for the canon EOS remote capture application in osx

--- StopMotion/src/main.cpp
+++ StopMotion/src/main.cpp
@@ -6,7 +6,7 @@ int main( ){
 
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
-	ofSetupOpenGL(400,600, OF_WINDOW);			// <-------- setup the GL context
+	ofSetupOpenGL(800,600, OF_WINDOW);			// <-------- setup the GL context
 	
 	
 	// this kicks off the running of my app
